72932d797fa4b953aac5389973d07349d62d090b
jxdang1 <dangjennifer21@gmail.com>
2024-02-20 18:52:12 -0600

    added necessary files for project

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App.jsx'
+import { routes } from './routes.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: routes.map((route) => ({
+      index: route.path === '/',
+      path: route.path === '/' ? undefined : route.path,
+      element: route.element,
+    })),
+  },
+])
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+)
