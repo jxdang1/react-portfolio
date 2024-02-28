0f5f012eed488a8937201507a300501deba91901
jxdang1 <dangjennifer21@gmail.com>
2024-02-27 19:04:08 -0600

    adjusted text to center, added images, debugging

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,17 +7,23 @@ const Portfolio = () => {
 
         <div className="flexContainer">
 
-          <Project name={'Project 3 Placeholder'} 
-            github={'http://github.com'}
-            deployed={'http://google.com'}
-            imageURL={'http://placekitten.com/200/200'}
-            description={'Placeholder for 3rd Group Project'} />
+          <Project name={'PWA J.A.T.E Text Editor'} 
+            github={'https://github.com/jxdang1/pwa-text-editor'}
+            deployed={'https://polar-river-77993-3eb0c051fa2e.herokuapp.com/'}
+            imageURL={'./jate-ss.jpg'}
+            description={'Single-page application that meets PWA criteria that is a text editor that runs in the browser'} />
           
-            <Project name={'E-Commerce Back End'} 
-              github={'https://github.com/jxdang1/ecommerce-backend'}
-              deployed={''}
-              imageURL={'http://placekitten.com/200/200'}
-              description={'Back end application for an e-commerce site using Express.js and Sequelize'} />
+            <Project name={'Weather Dashboard'} 
+              github={'https://github.com/jxdang1/weather-dashboard'}
+              deployed={'https://jxdang1.github.io/weather-dashboard/'}
+              imageURL={'./weather-ss.jpg'}
+              description={'Weather application that uses OpenWeatherMaps geocoding API to input city of choice to see the forecast of city.'} />
+
+            <Project name={'Code Quiz'} 
+              github={'https://github.com/jxdang1/code-quiz'}
+              deployed={'https://jxdang1.github.io/code-quiz/'}
+              imageURL={'./code-quiz-ss.jpg'}
+              description={'Created a simple code quiz that tests users knowledge on different languages with the use of JavaScript.'} /> 
         </div>
       </div>
     );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,8 @@
 const Resume = () => {
     return (
-      <div>
+      <div className="formInput">
         <h2>Resume</h2>
-        <p>Download resume <a href=''>here!</a></p>
+        <p>Download resume <a href='./Jennifer Dang - Resume.pdf'>here!</a></p>
         <h3>Front End Proficiencies:</h3>
         <ul>
           <li>HTML</li>
@@ -18,6 +18,15 @@ const Resume = () => {
           <li>Express</li>
           <li>MySQL & Sequelize</li>
           <li>MongoDB & Mongoose</li>
+        </ul>
+        <h3>Other Skill Sets:</h3>
+        <ul>
+          <li>Scanning Electron Microscrope</li>
+          <li>X-Ray Fluorescence</li>
+          <li>Laser Ablation</li>
+          <li>Chemical Decapsulation</li>
+          <li>Destructive Bond Pull Testing</li>
+          <li>Analyzing Military Standards such as MIL-STD-883, MIL-STD-750, etc</li>
         </ul>
       </div>
     );
