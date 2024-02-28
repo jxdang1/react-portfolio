31d940ce9bd1c2db5aa524716c9787730db3e843
jxdang1 <dangjennifer21@gmail.com>
2024-02-27 19:47:28 -0600

    more debugging

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,9 +8,8 @@ const About = () => {
           <p className="about">
             Hello, I'm Jennifer Dang. As a Component Lab Analyst at Integra Technologies Inc., I apply my strong background in quality assurance and failure analysis to ensure the reliability and performance of semiconductors. I use various testing methods and tools to identify and resolve defects, faults, and failures in complex electronic components and systems. I also collaborate with engineers, technicians, and customers to provide technical support and feedback on the analysis results and recommendations.
             
-            <br /> <br /> In addition to my analytical skills, I have a passion for web development. I am currently pursuing a certificate in full stack web development at the University of Kansas, where I am learning to master the intricacies of web technologies, such as HTML, CSS, JavaScript, Node.js, React, and MongoDB. I am eager to create seamless and user-friendly web applications that solve real-world problems and enhance user experience. My goal is to combine my knowledge and skills in semiconductors and web development to contribute to the dynamic and innovative world of technology.
+            <br /><br /> In addition to my analytical skills, I have a passion for web development. I am currently pursuing a certificate in full stack web development at the University of Kansas, where I am learning to master the intricacies of web technologies, such as HTML, CSS, JavaScript, Node.js, React, and MongoDB. I am eager to create seamless and user-friendly web applications that solve real-world problems and enhance user experience. My goal is to combine my knowledge and skills in semiconductors and web development to contribute to the dynamic and innovative world of technology.
           </p>
-          <p>Checkout my resume for more information!</p>
       </div>
     );
   }
